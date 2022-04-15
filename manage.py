450d300de94e97cbf5da9e39a007c8bce2eba63d
Salim Dason <dasonsalim@outlook.com>
2022-04-15 13:27:24 +0000

    add .env path

---
+++ manage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+import pathlib
+import dotenv
+
+
+def main():
+    """Run administrative tasks."""
+    DOT_ENV_PATH = pathlib.Path() / '.envs/.env'
+    if DOT_ENV_PATH.exists():
+        dotenv.read_dotenv(str(DOT_ENV_PATH))
+    else:
+        print("No .env found make sure to verify the path or create one if missing.")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controller.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
